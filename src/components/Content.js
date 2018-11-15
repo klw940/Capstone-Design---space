5724f7d2b990a64b32b140f6867ca1ed783245e8
zongog <zongog@ajou.ac.kr>
2018-11-15 21:34:33 +0900

    2018-11-15 Sale기본화면Content.js수정

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,6 +9,13 @@ import QnABoard from "./QnABoard/QnABoard";
 import Login from "./Login/Login"
 import SignUp from "./SignUp/SignUp";
 import WriteFree from "./FreeBoard/WriteFree";
+import antenna_sale from "./Sale/antenna_sale";
+import battery_sale from "./Sale/battery_sale";
+import cb_sale from "./Sale/cb_sale";
+import esc_sale from "./Sale/esc_sale";
+import frame_sale from "./Sale/frame_sale";
+import motor_sale from "./Sale/motor_sale";
+import wings_sale from "./Sale/wings_sale";
 const Content = () =>{
     return(
         <Switch>
@@ -21,6 +28,13 @@ const Content = () =>{
             <Route path="/Write-Free-Board" component={WriteFree}/>
             <Route path="/login" component={Login}/>
             <Route path="/sign-up" component={SignUp}/>
+            <Route path="/antenna_sale" component={antenna_sale}/>
+            <Route path="/battery_sale" component={battery_sale}/>
+            <Route path="/cb_sale" component={cb_sale}/>
+            <Route path="/esc_sale" component={esc_sale}/>
+            <Route path="/frame_sale" component={frame_sale}/>
+            <Route path="/motor_sale" component={motor_sale}/>
+            <Route path="/wings_sale" component={wings_sale}/>
         </Switch>
     )
 }
