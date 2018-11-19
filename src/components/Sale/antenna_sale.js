8368e0d5f29fbaabeea53ee7d1e3ddfdf86b0f9e
Kwon <klw95@naver.com>
2018-11-19 15:46:59 +0900

    부품 판매 UI 추가

---
+++ src/components/Sale/antenna_sale.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Label, Input } from 'reactstrap';
+
+class antenna_sale extends Component{
+    render(){
+        return(
+            <div>
+                <form action="" method="post" encType="multipart/form-data">
+                    <table className="table">
+                        <tr>
+                            <th>제품명</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>섬네일(업로드)</th>
+                            <td>
+                                <input type="file" name="motor_image"/>
+                                <a href="/uploads/<%=motor.motor_image%>" target="_blank">업로드 이미지 보기</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>가격</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>무게</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>신호 강도</th>
+                            <td>
+                                <Input type="select" name="select">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>길이(가로)</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>길이(세로)</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>재고</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>비고</th>
+                            <td>
+                                <Input type="textarea" name="text" />
+                            </td>
+                        </tr>
+                    </table>
+                    <button className="btn btn-primary">등록</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default antenna_sale;
---
+++ src/components/Sale/esc_sale.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import {Input} from "reactstrap";
+
+class esc_sale extends Component{
+    render(){
+        return(
+            <div>
+                <form action="" method="post" encType="multipart/form-data">
+                    <table className="table">
+                        <tr>
+                            <th>제품명</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>섬네일(업로드)</th>
+                            <td>
+                                <input type="file" name="motor_image"/>
+                                <a href="/uploads/<%=motor.motor_image%>" target="_blank">업로드 이미지 보기</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>가격</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>무게</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>길이(가로)</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>길이(세로)</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>재고</th>
+                            <td>
+                                <Input type="text" name="text" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>비고</th>
+                            <td>
+                                <Input type="textarea" name="text" />
+                            </td>
+                        </tr>
+                    </table>
+                    <button className="btn btn-primary">등록</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default esc_sale;
