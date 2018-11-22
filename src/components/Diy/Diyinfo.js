e85b9d256be54c8a92da7ad892549022095d2f80
Kwon <klw95@naver.com>
2018-11-23 02:29:28 +0900

    부품 목록 출력

--- src/components/Diy/Diyinfo.js
+++ src/components/Diy/Diyinfo.js
@@ -5,16 +5,7 @@ import {
     } from 'reactstrap';
 
 class DiyInfo extends Component {
-    static defaultProps = {
-        info: {
-            img: '',
-            price: '',
-            part: '',
-            name: '',
-            description: '',
-            id: 0
-        },
-    }
+
     newWindow = () => {
         window.open('http://localhost:3000/', 'sharer', 'toolbar=0,status=0,width=548,height=325');
     }
@@ -31,18 +22,19 @@ class DiyInfo extends Component {
             margin: '8px'
         };
         const {
-            name, img, description, price
-        } = this.props.info;
+            info,
+        } = this.props;
+        console.log(info);
         return (
             <div style={style}>
                 <div onClick={this.newWindow}>
                     <Row>
                         <Col md="3" xs="4" sm="5">
-                    <img src={img} width="100%" alt={name}/>
+                    <img src={info.image} width="100%" alt={info.name}/>
                         </Col>
                         <Col>
-                    <div><b>{name}</b>  {price}원</div>
-                    <div>{description}</div>
+                            <div><b>{info.name}</b>  {info.price}원</div>
+                            <div>{info.description}</div>
                         </Col>
                     </Row>
                 </div>
